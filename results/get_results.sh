5074bac171f58ca57fe47aae1f0ce7ae89df7d39
cchauve-canexia <cchauve@canexiahealth.com>
2021-03-25 01:04:44 +0000

    last results

--- results/get_results.sh
+++ results/get_results.sh
@@ -5,6 +5,6 @@ do
   aws s3 cp s3://cchauve-orchestration-ch/altera-pilot-study-canexia/${SAMPLE} . --exclude "*" --include "*indels*.vcf.tar.gz" --recursive
   tar xzvf ${SAMPLE}_indels_filtered_snpeff.vcf.tar.gz
   tar xzvf ${SAMPLE}_indels_unfiltered.vcf.tar.gz
-  mv ${SAMPLE}_indels_filtered_snpeff.vcf.tar.gz ${SAMPLE}/
-  mv ${SAMPLE}_indels_unfiltered.vcf.tar.gz ${SAMPLE}/
+  rm ${SAMPLE}_indels_filtered_snpeff.vcf.tar.gz
+  rm ${SAMPLE}_indels_unfiltered.vcf.tar.gz
 done
